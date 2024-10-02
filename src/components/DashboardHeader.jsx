14a0ad162e0eddcd57e00d54d253db200dd7c5ad
c-dongon <christian.dongon02@gmail.com>
2024-10-02 14:53:48 -0700

    mobile adjustments

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -3,6 +3,8 @@ import LogoutButton from './LogoutButton';
 
 function DashboardHeader() {
     const [userName, setUserName] = useState("Guest");
+    const [userImage, setUserImage] = useState(null);
+
 
     useEffect(() => {
         const fetchUserName = async () => {
@@ -20,6 +22,10 @@ function DashboardHeader() {
                         const data = await response.json();
                         setUserName(data.display_name || 'Guest');
                         localStorage.setItem('userName', data.display_name || 'Guest');
+                        
+                        if (data.images && data.images.length > 0) {
+                            setUserImage(data.images[0].url);
+                        }
                     } else {
                         console.error('Failed to fetch user data');
                     }
@@ -36,6 +42,13 @@ function DashboardHeader() {
             <div className="dashboard-header-left">
                 <h1>Vibify</h1>
                 <p>Hello, {userName}!</p>
+                {userImage && (
+                    <img 
+                        src={userImage} 
+                        alt="User profile" 
+                        className="user-profile-img"
+                    />
+                )}
             </div>
             <div className="dashboard-header-right">
                 <LogoutButton />
